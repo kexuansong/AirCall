3c7171892fdca2cdee1a60de9857f350abc7ac76
kexuansong <kexuansong@gmail.com>
2022-02-05 23:45:23 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
+import './css/callRecord.css';
+import './css/callDetails.css';
 
 import App from './App.jsx';
-
